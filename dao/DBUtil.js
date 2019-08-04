4789f3fb881a9c3ae8053e8669e4fe812c315692
chun <1173182575@qq.com>
2019-08-05 07:55:39 +0800

    init

--- dao/DBUtil.js
+++ dao/DBUtil.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 function createConnection() {
     var connection = mysql.createConnection({
-        host: "192.168.1.6",
+        host: "120.27.251.43",
         post: "3306",
         user: "root",
         password: "123456",
